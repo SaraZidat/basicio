74a1b9116d6b3875199203f793b6bcad3cc2d78b
ZIDAT <zidatsara@gmail.com>
2019-06-14 22:21:08 +0200

    Exercise 7 version2

--- src/Exercise7.java
+++ src/Exercise7.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public class Exercise7 {
     public static void main(String[] args) throws IOException {
-        String dir = System.getProperty("java.io.dir");
+        String dir = System.getProperty("java.io.tmpdir");
         for (int i = 1; i <= 10; i++){
             Path path = Paths.get(dir, i+".txt");
             new FileWriter(path.toFile());
